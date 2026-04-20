85c98fca1c0b8ae689c457b099604379467e9771
JOKANewDev <lopajoka@gmail.com>
2026-04-19 22:13:37 -0300

    Mudanca de local

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, BackgroundTasks, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from backend.main import DOWNLOAD_DIR
 from pydantic import BaseModel
 import uuid
 import os
